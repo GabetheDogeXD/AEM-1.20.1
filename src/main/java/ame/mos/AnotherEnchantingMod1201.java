2d74f94b6df32364df17a0c662d3b43bd0fc4454
GabetheDogeXD <ericgonzalez03@icloud.com>
2025-07-18 17:51:58 -0700

    Initial commit

---
+++ src/main/java/ame/mos/AnotherEnchantingMod1201.java
@@ -0,0 +1,17 @@
+package ame.mos;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AnotherEnchantingMod1201 implements ModInitializer {
+	public static final String MOD_ID = "another-enchanting-mod-1201"; 
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/ame/mos/AnotherEnchantingMod1201Client.java
@@ -0,0 +1,10 @@
+package ame.mos;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AnotherEnchantingMod1201Client implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
